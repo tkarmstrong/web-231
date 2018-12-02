72474f34a677c48f5e63d51bf1121e25f2b59e05
“tkarmstrong” <tkarmstrong@my365.bellevue.edu>
2018-12-02 17:36:15 -0500

    Adding variables & output exercise

---
+++ week-1/variables-values-output.js
@@ -0,0 +1,32 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "Tyler";
+var lastName = "Armstrong";
+var age = 36;
+var street = "Some Street";
+var city = "Some City";
+var state = "VA";
+var zipCode = "22202";
+
+// output 
+console.log(
+    firstName + ' ' + lastName + '\n' +
+    street + '\n' + 
+    city + ', ' + state + '  ' + zipCode + '\n'
+); 
+
+
+// end program 
