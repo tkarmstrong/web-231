666db735797cc20a7a92c7cf164ea9b1937da2a9
“tkarmstrong” <tkarmstrong@my365.bellevue.edu>
2019-01-20 18:19:25 -0500

    Added armstrong-assignment-6.4.js

---
+++ week-6/armstrong-assignment-6.4.js
@@ -0,0 +1,40 @@
+/*
+============================================
+; Title:  Assignment 6.4 - Nested Object Literals
+; Author: Tyler Armstrong
+; Date:   20 January 2019
+; Modified By: N/A
+; Description: Showcases nested object literals.
+;===========================================
+*/
+
+/**
+* Params: firstName, lastName, assignment
+* Response: output 
+* Description: Returns a well-formatted string header
+*/
+const header = require('../armstrong-header');
+
+console.log(header.display("Tyler", "Armstrong", "Exercise 6.3") + '\n');
+
+// object 
+const ticket = {
+  id: 105,
+  name: "Help Desk Support",
+  dateCreated: new Date().toLocaleDateString('en-US'),
+
+  person: {
+    id: 1,
+    firstName: "Bob",
+    lastName: "Jones",
+    jobTitle: "Programmer I"
+  }
+}
+
+// output to match starter code
+console.log(`Ticket ${ticket.id} was created on ${ticket.dateCreated} and assigned to employee ${ticket.person.firstName} ${ticket.person.lastName} (${ticket.person.jobTitle}).`);
+
+
+  
+  
+  
